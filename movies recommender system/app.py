c0f9679c8e09cb6ed7956dd10b82eb1cd9078fb3
Samirsaiyed <97721836+Samirsaiyed@users.noreply.github.com>
2023-04-27 12:53:29 +0530

    done

---
+++ movies recommender system/app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+import pickle
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommend_movies = []
+    for i in movies_list:
+        movie_id = i[0]
+        # fetch psoter from API
+        recommend_movies.append(movies.iloc[i[0]].title)
+    return recommend_movies
+
+
+movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity =  pickle.load(open('similarity_1.pkl','rb'))
+
+st.title('Movie Recommender Sysytem')
+
+selected_movie_name = st.selectbox(
+'How would you like to be contacted ?',
+movies['title'].values
+)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
